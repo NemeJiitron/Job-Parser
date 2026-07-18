92d120919ebcad4038d06d91c6c42b0ce8a7577d
NemeJiitron <m.lukarevskiy@gmail.com>
2026-07-18 16:24:52 +0300

    replace get-requests with get_offers_by

--- app/router/jobs.py
+++ app/router/jobs.py
@@ -11,16 +11,16 @@ from app.database import get_db
 
 router = APIRouter(prefix="/jobs", tags=['jobs'])
 
-
 @router.get("/", response_model=List[JobResponse])
-def get_offers(db: Session = Depends(get_db)):
-    jobs = db.query(Job).all()
-    return jobs
-
-@router.get("/{keyword}", response_model=List[JobResponse])
-def get_offers_by_keyword(keyword: str, db: Session = Depends(get_db)):
-    jobs = db.query(Job).filter(Job.keyword == keyword).all()
-    return jobs
+def get_offers_by(location: str | None = None, source: str | None = None, keyword: str | None = None, db: Session = Depends(get_db)):
+    jobs = db.query(Job)
+    if location:
+        jobs = jobs.filter(Job.location == location)
+    if source:
+        jobs = jobs.filter(Job.source == source)
+    if keyword:
+        jobs = jobs.filter(Job.keyword == keyword)
+    return jobs.all()
 
 @router.post("/parse", response_model=str)
 def parse_jobs(keyword: str, location: str, source: str, db: Session = Depends(get_db)):
